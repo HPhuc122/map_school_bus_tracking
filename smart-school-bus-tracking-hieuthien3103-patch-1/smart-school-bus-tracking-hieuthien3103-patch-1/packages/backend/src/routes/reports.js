b1fe2d7f0a0ea3a752990bb55cefb954e8494456
HPhuc122 <phuc33145@gmail.com>
2025-11-12 14:55:51 +0700

    Delete smart-school-bus-tracking-hieuthien3103-patch-1/smart-school-bus-tracking-hieuthien3103-patch-1 directory

--- smart-school-bus-tracking-hieuthien3103-patch-1/smart-school-bus-tracking-hieuthien3103-patch-1/packages/backend/src/routes/reports.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reportsController = require('../controllers/reportsController');
-
-router.get('/overview', reportsController.getOverview);
-router.get('/schedules', reportsController.getScheduleReport);
-router.get('/drivers', reportsController.getDriverPerformance);
-
-module.exports = router;
--- smart-school-bus-tracking-hieuthien3103-patch-1/smart-school-bus-tracking-hieuthien3103-patch-1/packages/shared/src/types/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// Central type definitions for Smart School Bus System
-Object.defineProperty(exports, "__esModule", { value: true });
